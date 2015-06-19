9b0a7f4d2b6369a7f17ee906d6e2444aaaa30ffb
Corey Light <coreylight@gmail.com>
2015-06-19 15:16:08 -0500

    organize and setup

--- public/js/src/app.js
+++ public/js/src/app.js
@@ -1,8 +1,8 @@
 (function(){
 
-  angular.module('opsee', ['ngTouch', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ui.router', 'ngAnimate', 'opsee.global'])
+  angular.module('opsee', ['ngTouch', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAnimate', 'angularMoment', 'opsee.global'])
 
-  angular.module('opsee').run(function ($rootScope, $window, $q, $http, $templateCache, $location, $timeout, $document, $analytics, $state) {
+  angular.module('opsee').run(function ($rootScope, $window, $q, $http, $templateCache, $location, $timeout, $document, $analytics) {
 
     $window.FastClick.attach(document.body);
 
--- public/js/src/global/services.js
+++ public/js/src/global/services.js
@@ -2,5 +2,9 @@
 
 angular.module('opsee.global.services', []);
 
+function moment($window){
+  return $window.moment;
+}
+angular.module('opsee.global.services').service('moment',moment);
 
 })();//IIFE
