5d22c4cdc041a37263874fdaae326c1390a1577d
Sara Bee <sara.bainbridge@gmail.com>
2016-01-28 15:33:30 -0500

    Use UUID v4

--- _site/public/js/app.js
+++ _site/public/js/app.js
@@ -13,15 +13,11 @@ function getAnonymousUUID() {
  * @returns {string} - a random UUID
  */
 function setAnonymousUUID() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  // @see http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-  var uuid = s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-
+  // @see http://stackoverflow.com/a/2117523
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
   localStorage.setItem('_opsee_uuid', uuid);
 }
 
